75196521a84a1191988aceaecc400265c29f99e1
vinu <Vinod.Challa@franklintempleton.com>
2023-05-22 14:29:24 +0530

    javascript file has updated in newBranch2

--- javascript.js
+++ javascript.js
@@ -1,2 +1,3 @@
 This is the first sample text file created by using the git bash command 'cat > filename.extension'.
 some changes are made after the changing the file name to javascript.js. here we made any changes if needed.
+Code will updated in this file in newBranch2 branch.
